429c5b00f1072f4e6c1ebb0805353073c2ce7a2a
Your Name <phillipkregg@gmail.com>
2012-07-20 16:57:16 -0500

    Commented out authorize attribute in franchiseset controller to troubleshoot

--- FranchiseManagerTest/Controllers/FranchiseSetController.cs
+++ FranchiseManagerTest/Controllers/FranchiseSetController.cs
@@ -8,7 +8,7 @@ using System.Configuration;
 
 namespace FranchiseManagerTest.Controllers
 {   
-    [Authorize(Roles="Administrator")]
+    //[Authorize(Roles="Administrator")]
 	public class FranchiseSetController : Controller
 	{
         
