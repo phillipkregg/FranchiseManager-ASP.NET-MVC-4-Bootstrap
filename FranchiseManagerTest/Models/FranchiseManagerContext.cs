9e5d5c887980bd47293b0036ea14161e789cc7fd
Your Name <phillipkregg@gmail.com>
2012-08-01 17:58:43 -0500

    Modified web.config and context class to utilize appharbor connection string

--- FranchiseManagerTest/Models/FranchiseManagerContext.cs
+++ FranchiseManagerTest/Models/FranchiseManagerContext.cs
@@ -9,10 +9,10 @@ namespace FranchiseManagerTest.Models
     public class FranchiseManagerContext : DbContext
     {
         // This constructor specifies the connection string name - needed for AppHarbor deploy
-        //public FranchiseManagerContext() : base("DefaultConnection")
-        //{
+        public FranchiseManagerContext() : base("DefaultConnection")
+        {
 
-        //}
+        }
         public DbSet<FranchiseSet> FranchiseSets { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
         public DbSet<User> Users { get; set; }
