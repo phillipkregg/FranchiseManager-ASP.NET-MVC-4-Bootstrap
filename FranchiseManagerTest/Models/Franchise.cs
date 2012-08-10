01bf88e1481e3a18dc7344e0b299db56381a75da
Your Name <phillipkregg@gmail.com>
2012-08-10 16:43:44 -0500

    Working on Franchise validation

--- FranchiseManagerTest/Models/Franchise.cs
+++ FranchiseManagerTest/Models/Franchise.cs
@@ -13,10 +13,10 @@ namespace FranchiseManagerTest.Models
         [ForeignKey("FranchiseSet")]
         public int FranchiseSetId { get; set; }
 
-        [Required(ErrorMessage = "Franchise Name is required.")]
+        //[Required(ErrorMessage = "Franchise Name is required.")]
         public string FranchiseName { get; set; }
 
-        [Required(ErrorMessage = "Franchise Number is required.")]
+        //[Required(ErrorMessage = "Franchise Number is required.")]
         public string FranchiseNumber { get; set; }        
         
         public virtual FranchiseSet FranchiseSet { get; set; }
